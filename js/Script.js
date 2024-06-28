cfba69d34c84dc9264ce643507fab7c50b5dcc18
GalalElaasar9 <138381852+GalalElaasar9@users.noreply.github.com>
2024-06-28 23:01:50 +0300

    .

--- js/Script.js
+++ js/Script.js
@@ -21,15 +21,48 @@ $(document).ready(function () {
 
 let scrolling = $(window).scrollTop();
 
-$(window).scroll(function () {
-  if (scrolling < $(window).scrollTop()) {
-    $(".navbar").slideUp();
-  } else {
-    $(".navbar").slideDown();
-  }
-  scrolling = $(window).scrollTop();
-});
+// $(window).scroll(function () {
+//   if (scrolling < $(window).scrollTop()) {
+//     $(".navbar").slideUp();
+//   } else {
+//     $(".navbar").slideDown();
+//   }
+//   scrolling = $(window).scrollTop();
+// });
 
+// let allLinks = document.querySelectorAll('.nav-item');
+
+// allLinks.forEach((link)=>{
+//   link.addEventListener('click',removeActive)
+// })
+
+// function removeActive() {
+//   allLinks.forEach((link)=>{
+//     link.classList.remove('active')
+//     this.classList.add('active')
+//   })
+// }
+
+let sections = document.querySelectorAll('section');
+let links = document.querySelectorAll('.nav-item');
+window.addEventListener('scroll',()=>{
+  sections.forEach(section =>{
+    let top = window.scrollY;
+    let offset = section.offsetTop - 40;
+    let id = section.getAttribute('id');
+    // if(top >= offset && top < offset + hight){
+    if(top >= offset){
+      links.forEach(link=>{
+        link.classList.remove('active');
+        let aHref  = document.querySelectorAll('ul li a[href*=' + id + ']');
+        console.log(aHref);
+        aHref.forEach(href=>{
+          href.parentElement.classList.add('active')
+        })
+      })
+    }
+  })
+})
 // let navItems = document.querySelectorAll(".nav-item");
 // let navbarCollapse = document.querySelector(".navbar-collapse");
 // let navbarToggler = document.querySelector(".navbar-toggler");
